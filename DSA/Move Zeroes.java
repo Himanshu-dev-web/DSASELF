0e78e1f34493e0e144b805e8730d63b8ef474e92
Himanshu Saini <55054295+Himanshu-dev-web@users.noreply.github.com>
2023-07-16 17:51:23 +0530

    Create Move Zeroes.java

---
+++ DSA/Move Zeroes.java
@@ -0,0 +1,34 @@
+
+
+<aside>
+💡 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the nonzero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+**Example 1:**
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+</aside>
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+    
+    int newsize = 0;
+    for(int i=0; i< nums.length ;i++){
+        if(nums[i] != 0)
+        {
+            nums[newsize] = nums[i];
+            newsize++;
+        }
+    }
+
+    for(int i=newsize; i< nums.length ;i++){
+       
+            nums[i] = 0;
+
+    }
+
+    }
+}
